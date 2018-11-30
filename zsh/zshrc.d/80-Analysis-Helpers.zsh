32b87ec8724fe500a4b7b21c1e66eadaf2002ff5
Jacob Steele <wxorxornull@google.com>
2018-11-30 00:23:30 -0800

    vim ctrlspace and signify, imwheel close tab, googler fix.

--- zsh/zshrc.d/80-Analysis-Helpers.zsh
+++ zsh/zshrc.d/80-Analysis-Helpers.zsh
@@ -2,7 +2,7 @@
 #                       Analysis Helper Aliases                       #
 #######################################################################
 
-alias g='googler'
+alias g='googler --noua'
 alias defangdomain='sed -e "s|\.|[\.]|g"'
 alias defangurl='sed -re "s|http(s?\:\/\/[[:alnum:]\.]*)\.([[:alnum:]]*\/?)|hxxp\1[.]\2|g"'
 
