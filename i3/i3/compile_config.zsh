2c90f4f568e865fed5c6988db29fa545b772c27a
Jacob Steele <wxorxornull@google.com>
2019-01-21 13:45:33 -0800

    Compile i3 config at dotbot install time.

---
+++ i3/i3/compile_config.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+#Zsh so we can do custom sorting w/o an external dependency.
+
+SOURCES=($HOME/(.dotfiles|.dotfiles_local)/i3/i3/*.conf(Noe\''REPLY=${REPLY:t}'\'))
+
+#echo $SOURCES
+
+# Concatenate all i3 files from global and local dotfiles into master config.
+mkdir -p ~/.config/i3/
+cat $SOURCES > ~/.config/i3/config
