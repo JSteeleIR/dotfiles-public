5b9ac645ffdc77c52835a977ebd719ce56fce73c
Jacob Steele <jsteele@jsteeleir.com>
2019-07-14 21:21:48 +0200

    add gcloud element for #p9k

--- zsh/zshrc.d/90-Powerlevel9k.zsh
+++ zsh/zshrc.d/90-Powerlevel9k.zsh
@@ -8,9 +8,24 @@ POWERLEVEL9K_TIME_FOREGROUND="white"
 
 POWERLEVEL9K_VIRTUALENV_BACKGROUND='013'
 POWERLEVEL9K_VIRTUALENV_FOREGROUND='015'
+
+# If gcloud has been used in the last 5 commands, pull the current active configuration information.
+zsh_p9k_gcloud(){
+    if [[ $(fc -nl -5) =~ 'gcloud|gc' ]]; then
+        if (( $+commands[gcloud] )); then
+            local config_name=$(gcloud config configurations list | awk '{if ($2 == "True") print $1;}')
+            local project_id=$(gcloud config configurations list | awk '{if ($2 == "True") print $4;}')
+            echo -n "\u2601 $config_name:$project_id"
+        fi
+    fi
+}
+POWERLEVEL9K_CUSTOM_GCLOUD="zsh_p9k_gcloud"
+POWERLEVEL9K_CUSTOM_GCLOUD_FOREGROUND="white"
+POWERLEVEL9K_CUSTOM_GCLOUD_BACKGROUND="018"
+
 # Disable kubecontext for now
 #POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs newline custom_date kubecontext time virtualenv)
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs newline custom_date time virtualenv)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs newline custom_date time custom_gcloud virtualenv)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vi_mode status history command_execution_time background_jobs load)
 
 # Two-lined prompt
@@ -22,3 +37,5 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vi_mode status history command_execution_tim
 #POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%K{black}%F{white} `date +%Y-%m-%d\ %T%Z` %f%k%F{093}\uE0B0%f "
 
 #POWERLEVEL9K_DISABLE_RPROMPT=true
+
+
--- zsh/zshrc.d/90-gcloud.zsh
+++ zsh/zshrc.d/90-gcloud.zsh
@@ -6,5 +6,14 @@
 if [[ -d ~/google-cloud-sdk ]]; then
     source ~/google-cloud-sdk/path.zsh.inc
     source ~/google-cloud-sdk/completion.zsh.inc
+fi
+
+if [[ -d /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/ ]]; then
+
+    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+fi
+
+if (($+commands[gcloud])); then
     alias gc=gcloud
 fi
