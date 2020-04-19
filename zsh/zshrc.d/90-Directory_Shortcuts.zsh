ceaf737ed0ab9c098d7d4b5eb79d2b4b11b16933
Jacob Steele <jsteele@jsteeleir.com>
2020-04-19 16:59:20 -0700

    Fix #zsh Directory Shortcuts typo.

--- zsh/zshrc.d/90-Directory_Shortcuts.zsh
+++ zsh/zshrc.d/90-Directory_Shortcuts.zsh
@@ -1,8 +1,8 @@
 # Set some shortcuts for common paths
 function hash_dirs() {
     echo "Updating directory hash table..."
-    hash -d df="$HOME/.dotflies"
-    hash -d dfl="$HOME/.dotflies_local"
+    hash -d df="$HOME/.dotfiles"
+    hash -d dfl="$HOME/.dotfiles_local"
 
     # Meta-named dirs (depend on prior nameddirs)
     hash -d dfz="$nameddirs[df]/zsh/zshrc.d/"
