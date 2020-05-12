246736e6f913458813698a0ae5e9d8e6adc4e105
Jacob Steele <jsteele@jsteeleir.com>
2020-05-12 09:14:19 -0700

    Reorder #p10k #zsh rprompt.

--- zsh/zshrc.d/90-Powerlevel9k.zsh
+++ zsh/zshrc.d/90-Powerlevel9k.zsh
@@ -66,12 +66,19 @@ typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
 # last prompt line gets hidden if it would overlap with left prompt.
 typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
   # =========================[ Line #1 ]=========================
+  kubecontext             # current kubernetes context (https://kubernetes.io/)
+  terraform               # terraform workspace (https://www.terraform.io)
+  aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
+  #aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
+  #azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
+  gcloud                  # google cloud cli account and project (https://cloud.google.com/)
+  google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
+  virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
   status                  # exit code of the last command
   command_execution_time  # duration of the last command
   background_jobs         # presence of background jobs
   direnv                  # direnv status (https://direnv.net/)
   asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-  virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
   anaconda                # conda environment (https://conda.io/)
   pyenv                   # python environment (https://github.com/pyenv/pyenv)
   goenv                   # go environment (https://github.com/syndbg/goenv)
@@ -94,13 +101,6 @@ typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
   #plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
   #phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
   #haskell_stack           # haskell version from stack (https://haskellstack.org/)
-  kubecontext             # current kubernetes context (https://kubernetes.io/)
-  terraform               # terraform workspace (https://www.terraform.io)
-  aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
-  #aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
-  #azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
-  gcloud                  # google cloud cli account and project (https://cloud.google.com/)
-  google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
   #nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
   #ranger                  # ranger shell (https://github.com/ranger/ranger)
   #nnn                     # nnn shell (https://github.com/jarun/nnn)
