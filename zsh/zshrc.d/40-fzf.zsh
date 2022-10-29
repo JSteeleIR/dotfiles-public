b1d5186f2ffd42bed6e40642d0a9c86b36cbe443
Jacob Steele <jsteele@jsteeleir.com>
2022-10-29 01:11:20 -0600

    Fiz #fzf zsh completion for #osx.

--- zsh/zshrc.d/40-fzf.zsh
+++ zsh/zshrc.d/40-fzf.zsh
@@ -7,6 +7,23 @@ if [[ -f /usr/share/doc/fzf/examples/completion.zsh ]]; then
     source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
 
+if [[ -x /usr/local/opt/fzf/bin/fzf ]]; then
+
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+      PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
+
 #######################################################################
 #                          END of 40-fzf-zsh                          #
 #######################################################################
