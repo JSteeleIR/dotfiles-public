31c45f8d7e9d82b8638152e8bdd0b9dd54b5e4ea
Jacob Steele <jsteele@jsteeleir.com>
2023-12-11 19:33:20 -0700

    Update #fzf config to also source completions in #zsh on Darwin.

--- zsh/zshrc.d/40-fzf.zsh
+++ zsh/zshrc.d/40-fzf.zsh
@@ -2,26 +2,32 @@
 #                             40-fzf.zsh                              #
 #######################################################################
 
-if [[ -f /usr/share/doc/fzf/examples/completion.zsh ]]; then
-    source /usr/share/doc/fzf/examples/completion.zsh
-    source /usr/share/doc/fzf/examples/key-bindings.zsh
+if [[ `uname` == 'Darwin' ]]; then
+     [ -f ~/.fzf.zsh  ] && source ~/.fzf.zsh
 fi
 
-if [[ -x /usr/local/opt/fzf/bin/fzf ]]; then
-
-    # Setup fzf
-    # ---------
-    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-      PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ `uname` == 'Linux' ]]; then
+    if [[ -f /usr/share/doc/fzf/examples/completion.zsh ]]; then
+        source /usr/share/doc/fzf/examples/completion.zsh
+        source /usr/share/doc/fzf/examples/key-bindings.zsh
     fi
 
-    # Auto-completion
-    # ---------------
-    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+    if [[ -x /usr/local/opt/fzf/bin/fzf ]]; then
 
-    # Key bindings
-    # ------------
-    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+        # Setup fzf
+        # ---------
+        if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+          PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+        fi
+
+        # Auto-completion
+        # ---------------
+        [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+        # Key bindings
+        # ------------
+        source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+    fi
 fi
 
 #######################################################################
